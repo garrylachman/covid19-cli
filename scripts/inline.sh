e4f0809a6ead0f7515f0d1e787c82e6bf337a961
Garry Lachman <garry@lachman.co>
2020-03-25 12:45:57 +0200

    inline source files use basedir

--- scripts/inline.sh
+++ scripts/inline.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+basedir=$( cd `dirname $0` && cd ../src; pwd )
+
 old_IFS=$IFS # save the field separator
 IFS=$'\n' # new field separator, the end of line
 while read -r line; do
     if [[ "$line" =~ (\.|source)\s+.+ ]]; then
         file="$(echo $line | cut -d' ' -f2)"
+        file=$(eval echo $file)
         printf '%s\n' "$(cat $file)"
     else
-
       printf '%s\n' "$line"
     fi
 done < "$1"
